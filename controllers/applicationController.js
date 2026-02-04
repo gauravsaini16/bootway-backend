14c84ede92b921a125bcd09769f369f6bcf63e12
gauravsaini16 <gauravsainialw0211@gmail.com>
2026-02-04 23:30:29 +0530

    fixed the error

--- controllers/applicationController.js
+++ controllers/applicationController.js
@@ -1,5 +1,5 @@
 const Application = require('../models/Application');
-const Job = require('../models/Job');
+const Job = require('../models/job');
 
 // @desc    Get all applications
 // @route   GET /api/applications
